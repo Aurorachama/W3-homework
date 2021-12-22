3a9ed5fa99fa9c8d83ff900b80bca439898850f0
Aurorachama <ronaldwong39@gmail.com>
2021-12-22 16:55:32 +1030

    char string added

--- script.js
+++ script.js
@@ -13,6 +13,12 @@ function writePassword() {
 // the password generation function
 function generatePassword() {
 
+  var upcase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowcase = "abcdefghijklmnopqrstuvwxyz"
+  var number = "0123456789";
+  // # , " and $ not included
+  var spchar = " !%'()*+,-./:;<=>?@[\]^_`{|}~";
+
 // Pseudocode/idea: either if moves on, else stops; or if stops else moves on
 // considerations: the clarity, organization of the code? the logic?
 
@@ -31,36 +37,36 @@ function generatePassword() {
   console.log(lengthPr);
 
   // first condition (length of password)
-    if (lengthPr >= 8 && lengthPr <= 128) {
+  if (lengthPr >= 8 && lengthPr <= 128) {
 
-      // debugging
-      console.log("length check pass");
+    // debugging
+    console.log("length check pass");
 
-      // second prompt (uppercase)
-      var upcasePr = window.confirm("click OK to include Uppercase Letter");
+    // second prompt (uppercase)
+    var upcasePr = window.confirm("click OK to include Uppercase Letter");
 
-      // debugging
-      console.log("upper case " + upcasePr);
+    // debugging
+    console.log("upper case " + upcasePr);
 
-      //third prompt (lowercase)
-      var lowcasePr = window.confirm("click OK to include Uppercase Letter");
+    //third prompt (lowercase)
+    var lowcasePr = window.confirm("click OK to include Uppercase Letter");
 
-      //debugging
-      console.log("lower case "+ lowcasePr);
+    //debugging
+    console.log("lower case "+ lowcasePr);
 
-      //fourth prompt (numeric)
-      var numPr = window.confirm("click OK to include Number");
+    //fourth prompt (numeric)
+    var numPr = window.confirm("click OK to include Number");
 
-      //dubugging
-      console.log("number "+ numPr);
+    //dubugging
+    console.log("number "+ numPr);
 
-      //fifth prompt (special character)
-      var spcharPr = window.confirm("click OK to include Special Characters");
+    //fifth prompt (special character)
+    var spcharPr = window.confirm("click OK to include Special Characters");
 
-      //debugging
-      console.log("special character "+ spcharPr);
-      
-      
+    //debugging
+    console.log("special character "+ spcharPr);
+    
+    for
 }
 
 
